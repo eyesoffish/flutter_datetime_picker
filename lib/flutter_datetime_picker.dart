345d012e5f02ff7f9cf1bc5e6c21c619d40cee82
zoulin <zoulin@supermonkey.com.cn>
2022-07-31 23:00:50 +0800

    提交

--- lib/flutter_datetime_picker.dart
+++ lib/flutter_datetime_picker.dart
@@ -309,7 +309,6 @@ class _DatePickerState extends State<_DatePickerComponent> {
               ),
               child: GestureDetector(
                 child: Material(
-                  color: theme.backgroundColor,
                   child: _renderPickerView(theme),
                 ),
               ),
